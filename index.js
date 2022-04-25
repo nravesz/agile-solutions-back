8aaa9cc4877b142213ff3ad3c45100ea7f1fa9e9
nravesz <nraveszani@fi.uba.ar>
2022-04-25 00:28:48 -0300

    Now the server accept localhost at port 3000

--- index.js
+++ index.js
@@ -27,6 +27,13 @@ const data = {
     }
 }
 
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 // get all categories, products and brands
 app.get("/api/options", (req, res) => {
     try {
